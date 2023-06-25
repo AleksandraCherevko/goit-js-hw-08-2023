f4a9d04f9f56b50ae104f9d7d90a13dcd8d09d19
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-25 16:51:06 +0200

    update task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(inputForm, 500));
+form.addEventListener('submit', submitForm);
+let formValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+fillForm();
+function fillForm() {
+  if (formValues) {
+    form.elements.email.value = formValues.email;
+    form.elements.message.value = formValues.message;
+  }
+}
+function inputForm() {
+  formValues = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formValues));
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+function submitForm(event) {
+  event.preventDefault();
+  console.log(formValues);
+  localStorage.removeItem(STORAGE_KEY);
+  event.currentTarget.reset();
+  formValues = {};
+}
 
