be2ced9721b14cec33192a88860ff4997d6a9cd7
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2023-06-25 16:41:20 +0200

    update 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,37 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imagesContainer = document.querySelector('.gallery');
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+imagesContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+console.log(galleryItems);
